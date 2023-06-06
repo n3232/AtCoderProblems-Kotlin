34765dac2bb0df93d5bf9c66078bbbc773553694
n3232 <kosukengo@gmail.com>
2023-06-06 20:47:04 +0900

    add 008_B

---
+++ src/main/kotlin/easy/008_B-121.kt
@@ -0,0 +1,40 @@
+package easy
+
+import kotlin.math.sqrt
+
+
+//https://atcoder.jp/contests/panasonic2020/tasks/panasonic2020_b
+fun main(args: Array<String>) {
+
+    val (a, b) = readLine()!!.split(" ")
+    val ab = (a + b).toDouble()
+    val ans = if (sqrt(ab) % 1.0 == 0.0) "Yes" else "No"
+    println(ans)
+
+//    val ab = (a + b).toInt()
+//    for (i in 0..317) {
+//        if (ab == i * i) {
+//            println("Yes")
+//            return
+//        }
+//    }
+//    println("No")
+}
+
+
+private fun readStr() = readLine()!! // string line
+private fun readInt() = readLine()!!.toInt() // single int
+private fun readStrings() = readLine()!!.split(" ") // list of strings
+private fun readInts() = readLine()!!.split(" ").map { it.toInt() } // list of ints
+
+// tips
+private fun stringList() = "abc".toList() // ["a", "b", "c"]
+//    println(a.joinToString("\n")) // each element of array/list of a separate line
+//val a = List(n) {
+//    readLine()!!.split(" ").map { it.toInt() }
+//}
+
+//for (i in 0 until n) {
+
+//test case
+//https://www.dropbox.com/sh/nx3tnilzqz7df8a/AAAYlTq2tiEHl5hsESw6-yfLa?dl=0
