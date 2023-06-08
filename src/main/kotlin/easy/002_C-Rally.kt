3f7462aece18bb6e8d0f6c790e3b57690e3d75e2
n3232 <kosukengo@gmail.com>
2023-06-08 20:42:50 +0900

    ann 002_B

--- src/main/kotlin/easy/002_C-Rally.kt
+++ src/main/kotlin/easy/002_C-Rally.kt
@@ -2,9 +2,16 @@ package easy
 
 //https://atcoder.jp/contests/abc156/tasks/abc156_c
 fun main() {
+    val n = readLine()!!.toInt()
+    val X = readLine()!!.split(" ").map { it.toInt() }
 
-    val n = readln().toInt()
-    val X = readln().split(" ").map { it.toInt() }
-    
-
+    var ans = Int.MAX_VALUE
+    for (p in 1.rangeTo(X.sortedDescending()[0])) {
+        var temp = 0
+        for (x in X) {
+            temp += (x - p) * (x - p)
+        }
+        if (temp < ans) ans = temp
+    }
+    println(ans)
 }
